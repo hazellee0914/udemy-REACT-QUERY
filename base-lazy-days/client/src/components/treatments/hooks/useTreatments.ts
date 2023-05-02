e150bb2ddacb0d93c72279f76ffb35226d405a37
hazellee0914 <starmagic2206@gmail.com>
2023-05-03 05:10:12 +0900

    fallback data

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,9 +1,9 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
-
-import { useQuery } from 'react-query';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -13,6 +13,7 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
+  const fallback = [];
   const { data } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
