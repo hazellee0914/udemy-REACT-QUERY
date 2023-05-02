78ce62c1b74e8baf9d1fc34aabba569f96bd542d
hazellee0914 <starmagic2206@gmail.com>
2023-05-02 18:37:10 +0900

    pagination 생성

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -4,22 +4,27 @@ import { useQuery } from 'react-query';
 import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
   // const data = [];
-  const { data, isError, isLoading } = useQuery('posts', fetchPosts, {
-    staleTime: 2000,
-  });
+  const { data, isError, isLoading } = useQuery(
+    // query key 가 바뀌면 useQuery 에 새로운 쿼리로 알려주고 데이터를 다시 가져옴.
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
   // fetchposts 가 해결될때까지 데이터는 거짓이되고, 해결되면 데이터에 배열이 포함 => 컴포넌트가 다시 렌더링되어 매핑!!
   if (isLoading) return <h3>Loading...</h3>;
   if (isError) return <h3>Oops, something went wrong</h3>;
@@ -38,11 +43,22 @@ export function Posts() {
         ))}
       </ul>
       <div className='pages'>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValus) => previousValus - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        {/* page1 가 일때 보여줌 */}
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValus) => previousValus + 1);
+          }}
+        >
           Next page
         </button>
       </div>
