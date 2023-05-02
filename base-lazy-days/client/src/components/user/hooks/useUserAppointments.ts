36b8fbc9a7a06a13e9d8c025b8ab562f1c05acd4
hazellee0914 <starmagic2206@gmail.com>
2023-05-03 08:27:00 +0900

    userAppointments

--- base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { useQuery } from 'react-query';
 
 import type { Appointment, User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -6,17 +7,26 @@ import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
 
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   user: User | null,
-// ): Promise<Appointment[] | null> {
-//   if (!user) return null;
-//   const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
-//     headers: getJWTHeader(user),
-//   });
-//   return data.appointments;
-// }
+// react 쿼리를 비활성하시키고, 유저가 거짓이면 쿼리함수 실행 x (null)
+async function getUserAppointments(
+  user: User | null,
+): Promise<Appointment[] | null> {
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
+    headers: getJWTHeader(user),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
   // TODO replace with React Query
-  return [];
+  const { user } = useUser();
+
+  const fallback: Appointment[] = [];
+  const { data: userAppointments = fallback } = useQuery(
+    'user-appointments',
+    () => getUserAppointments(user),
+    { enabled: !!user },
+  );
+  return userAppointments;
 }
